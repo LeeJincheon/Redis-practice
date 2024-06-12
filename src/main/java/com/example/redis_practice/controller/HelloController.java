40acc1211da6097d0d8c157ba64b47c8e2287999
LeeJincheon <pressky99@gmail.com>
2024-06-12 23:00:35 +0900

    redis 연동 컨트롤러 작성

---
+++ src/main/java/com/example/redis_practice/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.redis_practice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class HelloController {
+
+    private final StringRedisTemplate redisTemplate;
+
+    @GetMapping("/setFruit")
+    public String setFruit(@RequestParam String name) {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("fruit", name);
+        return "saved";
+    }
+
+    @GetMapping("/getFruit")
+    public String setFruit() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        return ops.get("fruit");
+    }
+}
