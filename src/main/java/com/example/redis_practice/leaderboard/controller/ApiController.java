e25abb9a198aeccf81965c02e0f6241af72e89f8
LeeJincheon <pressky99@gmail.com>
2024-06-14 02:27:18 +0900

    Leaderboard 구현

---
+++ src/main/java/com/example/redis_practice/leaderboard/controller/ApiController.java
@@ -0,0 +1,32 @@
+package com.example.redis_practice.leaderboard.controller;
+
+import com.example.redis_practice.leaderboard.service.RankingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ApiController {
+
+    private final RankingService rankingService;
+
+    @GetMapping("/setScore")
+    public Boolean setScore(@RequestParam String userId,
+                            @RequestParam int score) {
+        return rankingService.setUserScore(userId, score);
+    }
+
+    @GetMapping("/getRank")
+    public Long getUserRank(@RequestParam String userId) {
+        return rankingService.getUserRanking(userId);
+    }
+
+    @GetMapping("/getTopRank")
+    public List<String> getTopRanks() {
+        return rankingService.getTopRank(3);
+    }
+}
